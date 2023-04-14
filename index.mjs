a598dc39ad1c9d65e20010f99ca428af3c4e63a1
Darlan Alves <me@darlanalv.es>
2023-04-14 23:43:15 +0200

    feat: add content generation

---
+++ index.mjs
@@ -0,0 +1,73 @@
+import { createServer } from 'http';
+import { request } from 'https';
+import { join } from 'path';
+import { createHash } from 'crypto';
+import { readFileSync, writeFileSync, existsSync } from 'fs';
+
+const completionsURL = process.env.COMPLETIONS_URL;
+const index = readFileSync('./index.html', 'utf8');
+const CWD = process.cwd();
+const recents = [];
+
+async function serve(req, res) {
+    if (req.url === '/') {
+        res.end(index
+            .replace('{content}', '<h1 class="text-3xl font-bold mb-6">Start your search here</h1>')
+            .replace('{recents}', generateRecents())
+        );
+        return;
+    }
+
+    const urlPath = req.url;
+    recents.unshift(urlPath);
+
+    if (recents.length > 100) {
+        recents.pop();
+    }
+
+    const hash = sha256(urlPath);
+    const cachePath = join(CWD, 'cache', hash);
+
+    if (existsSync(cachePath)) {
+        const cached = readFileSync(cachePath, 'utf8');
+        res.end(cached);
+        return;
+    }
+
+    const article = await generate(urlPath);
+    const newContent = `<!-- ${urlPath} -->\n\n${article}`;
+ 
+    writeFileSync(cachePath, newContent);
+    
+    res.end(index
+        .replace('{content}', newContent)
+        .replace('{recents}', generateRecents())
+    );
+}
+
+function sha256(value) {
+    return createHash('sha256').update(value).digest('hex');
+}
+
+function generateRecents() {
+    return recents.map(next => `<li>${next}</li>`);
+}
+
+async function generate(urlPath) {
+    const prompt = `Create an HTML article that matches the following URL path: "${urlPath}".
+Add relative href links in the content that point to related topics or tags.
+Use semantic and SEO optimized markup and format it using Tailwind typography styles.
+Generate only the content, not the HTML page around it.
+At the end of the article, provide a list with links related to the current page and the sources from where the article was generated`
+    const payload = { messages: [{ role: 'user', content: prompt }]}
+    const options = { 
+        method: 'POST', 
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload),
+    };
+
+    const completion = await fetch(completionsURL, options).then((x) => x.json());
+    return completion.content;
+}
+
+createServer(serve).listen(process.env.PORT);
--- src/view/interpolate.ts
+++
@@ -1,7 +0,0 @@
-const INTERPOLATE_MATCHER = /\{\{\s*?([\S=]+?)\s*?\}\}/g;
-
-export class Interpolation {
-  static interpolate(template: string, variables: { [k: string]: any; } ) {
-    return template.replace(INTERPOLATE_MATCHER, (_, key) => key in variables ? variables[key] : '');
-  }
-}
